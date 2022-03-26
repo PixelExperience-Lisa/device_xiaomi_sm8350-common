e658be7a3f9a153a8ddeba74790d5c2a1c28fd23
AdarshGrewal <adarshgrewal@gmail.com>
2022-03-27 04:28:29 +0530

    sm8350-common: Import stock public libraries list
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- common.mk
+++ common.mk
@@ -417,6 +417,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
