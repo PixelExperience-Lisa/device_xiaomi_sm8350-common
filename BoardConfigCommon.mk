8a0f7a7ce09e9da4c0a36f38e0b04149ac0eba3c
NextWork123 <nextdesigns@libero.it>
2022-10-11 20:46:51 +0200

    sm8350-common: Compile HWUI for performance
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,9 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lineage.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
