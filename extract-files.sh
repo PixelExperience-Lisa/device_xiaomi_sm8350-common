91ebfad74d9a2217ae29948b36126bff0a574bee
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-20 02:44:34 +0530

    sm8350-common: Bring back dolby secure decoders and fix crash
    Seems like new changes to c2 libs in android 12,
    broke these decoders. Suspicious changes:
    https://github.com/AOSPA/android_frameworks_av/commit/34821f568bc39602752ce056e7c4df57d12f6459
    https://github.com/AOSPA/android_frameworks_av/commit/34821f568bc39602752ce056e7c4df57d12f6459
    https://github.com/AOSPA/android_frameworks_av/commit/1d5b3f9539ccfd0b77a07a446cb0595c221497ce
    
    Copy the old libs and patch dolby binary to load
    it, in order to fix/workaround this problem.
    
    While we're at it, drop separate c2 manifest and
    add the dolby instance in the main manifest itself.
    
    This reverts commits b6bf2345d3b687701ad626d34f58cf1ea4c9f966 and b9848af2ca6a87fbce95cee101d193e453e21923.
    
    Test: Dolby Vision playback in Netflix.
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,7 @@ function blob_fixup() {
             ;;
         vendor/bin/hw/dolbycodec2)
             patchelf --replace-needed libavservices_minijail_vendor.so libavservices_minijail.so "${2}"
+            patchelf --replace-needed libcodec2_hidl@1.0.so libcodec2_hidl@1.0.stock.so "${2}"
             ;;
         vendor/etc/camera/pureShot_parameter.xml \
         |vendor/etc/camera/pureView_parameter.xml)
@@ -75,9 +76,6 @@ function blob_fixup() {
         |vendor/etc/media_yupik_v1/video_system_specs.json)
             sed -i "/max_retry_alloc_output_timeout/ s/10000/0/" "${2}"
             ;;
-        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
-            sed -ni '/ozoaudio/!p' "${2}"
-            ;;
         vendor/lib64/android.hardware.secure_element@1.0-impl.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
@@ -87,6 +85,13 @@ function blob_fixup() {
 	vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8D0A0094AE1640F9/1F2003D5AE1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
             mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
+        vendor/lib/libcodec2_hidl@1.0.stock.so)
+            patchelf --set-soname libcodec2_hidl@1.0.stock.so "${2}"
+            patchelf --replace-needed libcodec2_vndk.so libcodec2_vndk.stock.so "${2}"
+            ;;
+        vendor/lib/libcodec2_vndk.stock.so)
+            patchelf --set-soname libcodec2_vndk.stock.so "${2}"
             ;;
         vendor/lib64/hw/camera.xiaomi.so)
             # Before
