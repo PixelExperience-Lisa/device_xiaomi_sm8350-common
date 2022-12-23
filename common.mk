610b612d9bdf780f1fdbf7b4e9c6b0f3b4cb5c3e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-23 05:55:35 +0530

    sm8350-common: Disable reserved size in gralloc handle
    Our stock blobs don't support this yet.
    
    12-22 13:23:37.485   942  1413 E qdmetadata: Invalid gralloc handle (at 0x795fe05768): ver(12/12) ints(22/23) fds(2/2)
    12-22 13:23:37.485   942  1413 E qdmetadata: validateAndMap: Private handle is invalid - handle:0x795fe05768
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- common.mk
+++ common.mk
@@ -178,6 +178,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := false
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
