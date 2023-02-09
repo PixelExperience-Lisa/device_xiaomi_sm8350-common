crdroidandroid/android_device_xiaomi_sm8350-common

sm8350-common: Hardcode LD and AR for custom clang
The LD and AR were hard-coded to CLANG_PREBUILTS, which is
clang-r450784d as of writing. If a device builds kernel with a
newer version of LLVM/Clang, the hard-coded LLD may encounter
unsupported features such as

ld.lld: error: built-in.o(init/main.o): Opaque pointers are only
supported in -opaque-pointers mode (Producer: 'LLVM15.0.0git'
Reader: 'LLVM 14.0.6git')

Therefore, only hard-code LD and AR when KERNEL_TOOLCHAIN is not set.

Change-Id: Ic707afc3ddf8101a734ccbd9f7a924952a20f6e3
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

BoardConfigCommon.mk
@@ -92,6 +92,8 @@ KERNEL_LD := LD=ld.lld
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin/ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS += AR=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin/llvm-ar
 TARGET_KERNEL_CLANG_COMPILE := true
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0

PixelExperience-Lisa/device_xiaomi_sm8350-common

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

BoardConfigCommon.mk
@@ -117,6 +117,8 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_CROSS_COMPILE_PREFIX_ARM32 := arm-linux-gnueabi-
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-custom
 KERNEL_SUPPORTS_LLVM_TOOLS := true
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-custom/bin/ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS += AR=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-custom/bin/llvm-ar
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom